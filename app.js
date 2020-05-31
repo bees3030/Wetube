1033cc1600bd78a99aa317df895d84c7436c3972
bees3030 <bees_3030@naver.com>
2020-05-31 17:19:34 +0900

    0531Savs

---
+++ app.js
@@ -0,0 +1,25 @@
+//const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+const app = express();
+
+//middleware
+app.use(cookieParser());
+//bodyparser이 아직 이해하기 힘들다
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
